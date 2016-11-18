c8b9115d3adea2f3961f969d21ead82c24d690fb
Nathan Sullivan <nathan@nightsys.net>
2016-11-18 22:43:56 +1000

    fix awserr import

--- shared/shared.go
+++ shared/shared.go
@@ -6,6 +6,7 @@ import (
 	"strings"
 
 	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/awserr"
 	"github.com/aws/aws-sdk-go/service/ec2"
 	"github.com/aws/aws-sdk-go/service/ecs"
 )
