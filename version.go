06b80d3a9a087879fc6b59982bf08ba7534a1bbd
Nathan Sullivan <nathan@nightsys.net>
2018-02-02 11:28:09 +1000

    Bump version.go for new release 0.4.4

--- version.go
+++ version.go
@@ -3,4 +3,4 @@ package main
 // NOTE: the script build_upload.sh manipulates this file with sed, ensure it still works
 // if this file is changed
 
-const ecs_discoverer_version = "0.4.3"
+const ecs_discoverer_version = "0.4.4"
